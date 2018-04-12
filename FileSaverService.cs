7330e258a87c480068c7b967509902e9a841a902
Lehalu <37669496+Lehalu@users.noreply.github.com>
2018-04-12 19:57:14 +0300

    good name
    good name

---
+++ FileSaverService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+namespace MKM
+{
+    public interface IFileSaverService
+    {
+        bool SaveFile(string content, string location, string fileName);
+    }
+
+    public class FileSaverService : IFileSaverService
+    {
+       
+        public bool SaveFile(string content, string location, string fileName)
+        {
+            bool result = false;
+
+            try
+            {
+                string newFileName = fileName.Replace(".txt", "_reverse.txt");
+
+                string newLocation = Path.Combine(Path.GetDirectoryName(location), newFileName);
+
+                File.Move(location, newLocation);
+                File.WriteAllText(newLocation, content);
+
+                result = true;
+            }
+            catch (FileNotFoundException)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("file location" + location + " not found.");
+                Console.ResetColor();
+            }
+            catch (IOException)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("Incorrect file name. ");
+                Console.ResetColor();
+            }
+
+            return result;
+        }
+    }
+}
