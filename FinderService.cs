a5a4d461f7a20a46af20ca6a024b3888c8ce6dc0
Светлана <geksogen4ik@gmail.com>
2018-04-12 19:59:57 +0300

    Add files via upload

---
+++ FinderService.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ModuleTwo
+{
+    public interface IFinderService
+    {
+        string FindFile(string locationToSearch, string fileName);
+    }
+
+    public class FinderService : IFinderService
+    {
+        private object directory;
+
+        public string FindFile(string locationToSearch, string fileName)
+        {
+            //Console.WriteLine("What file do you search?");
+            fileName = "CatchMeIfYouCan.txt";
+            locationToSearch = @"D:\";
+
+            foreach (string file in Directory.GetFiles(locationToSearch))
+            {
+                if (new FileInfo(file).Name.Equals(fileName))
+                {
+                    Console.WriteLine(file);
+                    Console.ReadKey();
+                    return file;
+                }
+            }
+            Console.ReadKey();
+            return "";
+
+        }
+
+    }
+
+}
+
+    
+
+
+            
+        
+    
+
+    
+        
+
+
+
+
+
+        
+     
+
+  
+
+
+
+    
---
+++ IModuleTwoInvoker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuleTwo
+{
+    public interface IModuleTwoInvoker
+    {
+        string FindFile(string locationToSearch, string fileName);
+
+        string GetFileContent(string location, string fileName);
+
+        bool SaveFile(string content, string lcoation, string fileName);
+    }
+}
